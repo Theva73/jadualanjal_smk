c8d304ce6337a25fcef80ef1a78475b367bbc834
Theva73 <tvarajan7@gmail.com>
2025-10-03 01:56:16 +0800

    Create form4to1_schedule.js

---
+++ form4to1_schedule.js
@@ -0,0 +1,99 @@
+// This file contains the schedule data for Form 4, 3, 2, and 1 classes.
+// It is loaded by the main unified_schedule_viewer.html file.
+
+const form4to1Schedules = [
+    {
+        className: "4SN1",
+        classTeacher: "PN. HANIM SURIA HUSSIN",
+        timeSlots: ["7:15-8:15", "8:15-9:15", "9:15-10:15", "10:15-10:30", "10:30-11:30", "11:30-12:30", "12:30-1:30", "1:30-2:30"],
+        rehatSlot: "10:15-10:30",
+        scheduleData: {
+            "ISNIN": [
+                { period: "7:15-8:15", subject: "MMT", teacher: "PN FAEZAH" },
+                { period: "8:15-9:15", subject: "FIZ", teacher: "PN. MALINA", location: "MAK. FIZ" },
+                { period: "9:15-10:15", subject: "BM", teacher: "CIK NAZA" },
+                { period: "10:30-11:30", subject: "BI", teacher: "MISS NIVETHA" },
+                { period: "11:30-12:30", subject: "KIM", teacher: "PN SYAMIMI", location: "MAK. KIM" },
+                { period: "12:30-1:30", subject: "PAI", teacher: "UST HAJAR" }
+            ],
+            "SELASA": [
+                { period: "7:15-8:15", subject: "BIO", teacher: "PN NOROSNIZA", location: "MAK. BIO" },
+                { period: "8:15-9:15", subject: "MM", teacher: "CIK ZURAIDAH" },
+                { period: "9:15-10:15", subject: "SEJ", teacher: "PN. ROSIEATI" },
+                { period: "10:30-11:30", subject: "KOKO" },
+                { period: "11:30-12:30", subject: "PJK", teacher: "EN AHMAD" },
+                { period: "12:30-1:30", subject: "FIZ", teacher: "PN. MALINA", location: "MAK. FIZ" }
+            ],
+            "RABU": [
+                { period: "7:15-8:15", subject: "BM", teacher: "CIK NAZA" },
+                { period: "8:15-9:15", subject: "MMT", teacher: "PN FAEZAH" },
+                { period: "9:15-10:15", subject: "BI", teacher: "MISS NIVETHA" },
+                { period: "10:30-11:30", subject: "MM", teacher: "CIK ZURAIDAH" },
+                { period: "11:30-12:30", subject: "BIO", teacher: "PN NOROSNIZA", location: "MAK. BIO" },
+                { period: "12:30-1:30", subject: "PAI", teacher: "UST HAJAR" }
+            ],
+            "KHAMIS": [
+                { period: "7:15-8:15", subject: "SEJ", teacher: "PN. ROSIEATI" },
+                { period: "8:15-9:15", subject: "KIM", teacher: "PN SYAMIMI", location: "MAK. KIM" },
+                { period: "9:15-10:15", subject: "PJK", teacher: "EN AHMAD" },
+                { period: "10:30-11:30", subject: "MMT", teacher: "PN FAEZAH" },
+                { period: "11:30-12:30", subject: "BM", teacher: "CIK NAZA" },
+                { period: "12:30-1:30", subject: "BI", teacher: "MISS NIVETHA" }
+            ],
+            "JUMAAT": [
+                { period: "7:15-8:15", subject: "MM", teacher: "CIK ZURAIDAH" },
+                { period: "8:15-9:15", subject: "KIMIA", teacher: "PN SYAMIMI", location: "MAK. KIM" },
+                { period: "9:15-10:15", subject: "FIZ", teacher: "PN. MALINA", location: "MAK. FIZ" },
+                { period: "10:30-11:30", subject: "BIO", teacher: "PN NOROSNIZA", location: "MAK. BIO" }
+            ]
+        }
+    },
+    {
+        className: "4AP1",
+        classTeacher: "PN. KARTINI YA'AKUB",
+        timeSlots: ["7:15-8:15", "8:15-9:15", "9:15-10:15", "10:15-10:30", "10:30-11:30", "11:30-12:30", "12:30-1:30", "1:30-2:30"],
+        rehatSlot: "10:15-10:30",
+        scheduleData: {
+            "ISNIN": [
+                { period: "7:15-8:15", subject: "BM", teacher: "CIK NASUHA" },
+                { period: "8:15-9:15", subject: "SEJ", teacher: "PN. KARTINI" },
+                { period: "9:15-10:15", subject: "SAINS", teacher: "EN. ROZALI" },
+                { period: "10:30-11:30", subject: "MM", teacher: "PN. IMELDA" },
+                { period: "11:30-12:30", subject: "BI", teacher: "PN. NURA" },
+                { period: "12:30-1:30", subject: "PAI", teacher: "UST FARIS" }
+            ],
+            "SELASA": [
+                { period: "7:15-8:15", subject: "PJK", teacher: "EN. SHARUL" },
+                { period: "8:15-9:15", subject: "KOKO" },
+                { period: "9:15-10:15", subject: "BM", teacher: "CIK NASUHA" },
+                { period: "10:30-11:30", subject: "MM", teacher: "PN. IMELDA" },
+                { period: "11:30-12:30", subject: "BI", teacher: "PN. NURA" },
+                { period: "12:30-1:30", subject: "SEJ", teacher: "PN. KARTINI" }
+            ],
+            "RABU": [
+                { period: "7:15-8:15", subject: "SAINS", teacher: "EN. ROZALI" },
+                { period: "8:15-9:15", subject: "BM", teacher: "CIK NASUHA" },
+                { period: "9:15-10:15", subject: "PAI", teacher: "UST FARIS" },
+                { period: "10:30-11:30", subject: "BI", teacher: "PN. NURA" },
+                { period: "11:30-12:30", subject: "MM", teacher: "PN. IMELDA" }
+            ],
+            "KHAMIS": [
+                { period: "7:15-8:15", subject: "SEJ", teacher: "PN. KARTINI" },
+                { period: "8:15-9:15", subject: "SAINS", teacher: "EN. ROZALI" },
+                { period: "9:15-10:15", subject: "PJK", teacher: "EN. SHARUL" },
+                { period: "10:30-11:30", subject: "BM", teacher: "CIK NASUHA" },
+                { period: "11:30-12:30", subject: "PAI", teacher: "UST FARIS" }
+            ],
+            "JUMAAT": [
+                { period: "7:15-8:15", subject: "BI", teacher: "PN. NURA" },
+                { period: "8:15-9:15", subject: "MM", teacher: "PN. IMELDA" },
+                { period: "9:15-10:15", subject: "SEJ", teacher: "PN. KARTINI" },
+                { period: "10:30-11:30", subject: "SAINS", teacher: "EN. ROZALI" }
+            ]
+        }
+    },
+    // You can add more class schedules for Forms 1, 2, and 3 here
+    // following the same structure.
+];
+
+
